7024b7ba1450429e90c2751016c5dda2cfec32e7
Jean-Christophe Dargere <jean-christophe.dargere@bull.net>
2018-01-05 17:25:52 -1000

    Initial Commit

---
+++ testbot/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  testbot
+//
+//  Created by JC DARGERE on 05/01/2018.
+//  Copyright © 2018 JC DARGERE. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
